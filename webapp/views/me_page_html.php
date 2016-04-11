abff04a2899af5f701685ba49268c905993f25ca
jordywardy <jordywardie@gmail.com>
2016-04-11 14:30:47 +0100

    Update me_page_html.php

--- webapp/views/me_page_html.php
+++ webapp/views/me_page_html.php
@@ -42,7 +42,7 @@
 			</div>
 		</div>
 
-	<form action="upload.php" method="post" enctype="multipart/form-data">
+	<form action="me_upload.php" method="post" enctype="multipart/form-data">
 		Select image to upload:
 		<input type="file" name="fileToUpload" id="fileToUpload">
 		<input type="submit" value="Upload Image" name="submit">
